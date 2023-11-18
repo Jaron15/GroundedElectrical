54c65d4880b05378b1ef3754e44eea3975562a7e
Jaron15 <jjkenyon777@gmail.com>
2023-11-18 13:52:37 -0700

    removed border

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,12 +8,12 @@ const fadeInVariant = {
 
 const Hero = () => {
   return (
-    <section className="bg-black text-white relative 2xl:mt-4 ">
-      <div className="container mx-auto text-center  ">
+    <section className="bg-black text-white relative 2xl:mt-4">
+      <div className="container mx-auto text-center">
 
         {/* Image Placeholder (your background image) with Text Inside */}
         <div className="relative">
-          <div className="absolute inset-0 bg-opacity-30 flex flex-col justify-between mt-4 py-2 h-[26rem] lg:h-[33rem] xl:h-[39rem] bg-hero bg-herobig border ">
+          <div className="absolute inset-0 bg-opacity-30 flex flex-col justify-between mt-4 py-2 h-[26rem] lg:h-[33rem] xl:h-[39rem] bg-hero bg-herobig">
 
             {/* Headlines */}
     <div className='ml-4 sm:ml-16 md:ml-14 lg:ml-36 xl:ml-44 2xl:ml-64  w-[95%] sm:w-5/6 md:w-11/12 lg:w-4/5 xl:w-4/5 2xl:w-[70%] mx-auto '>
