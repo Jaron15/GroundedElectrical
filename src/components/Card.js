2aba116758498b45385df15ecfbe8c034c42c3d6
Jaron15 <jjkenyon777@gmail.com>
2023-11-25 20:20:35 -0700

    card style/image change

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const Card = () => {
+const Card = ({name, title, description}) => {
   // Variants for the hover state
   const hoverVariants = {
     hover: {
       scale: 2.5,
       rotate: 20,
-      filter: 'blur(7px)',
+      filter: 'blur(7px) ' ,
       opacity: 1,
       translateY: 0,
+      transition: {
+        duration: .4
+      }
     },
   };
 
   return (
-    <div className="relative w-full h-full rounded-lg overflow-hidden cursor-pointer">
+    <div className="relative w-full h-full rounded-lg overflow-hidden cursor-pointer border">
       <motion.div 
         className="w-full h-full flex flex-col justify-center items-center"
         whileHover="hover"
@@ -28,25 +31,25 @@ const Card = () => {
         {/* Card Content with Background Image */}
         <motion.div 
           className="w-full h-full bg-cover bg-center flex justify-center items-center"
-          style={{ backgroundImage: 'url("/commercial.jpeg")' }}
+          style={{ backgroundImage: `url("/${name}.jpeg")` }}
           variants={hoverVariants}
         >
-          <div className='font-semibold text-2xl'>
-          Commercial
+          <div className='font-bold text-2xl text-shadow '>
+          {name}
           </div>
         </motion.div>
 
         {/* Text Content */}
         <motion.div 
-          className="absolute top-0 left-0 w-full h-full flex flex-col justify-center gap-2.5 text-gray-200 p-5 text-center"
+          className="absolute top-0 left-0 w-full h-full flex flex-col justify-center gap-2.5 text-zinc-200 p-5 text-center "
           initial={{ opacity: 0, translateY: 50 }}
           variants={{
             hover: { opacity: 1, translateY: 0, transition: { duration: 0.6 } }
           }}
         >
-          <h2 className="text-4xl font-bold">Card Hover</h2>
+          <h2 className="text-4xl font-bold ">{title}</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipii voluptas ten mollitia pariatur odit, ab minus ratione adipisci accusamus vel est excepturi laboriosam magnam necessitatibus dignissimos molestias.
+            {description}
           </p>
         </motion.div>
       </motion.div>
--- src/components/Services.js
+++ src/components/Services.js
@@ -6,25 +6,14 @@ function Services() {
     <div className='w-full py-16'>
         <div className='w-full flex flex-col items-center'>
             <h1 className='underline text-3xl font-semibold'>Services</h1>
-            {/* -----------HERE----------- */}
             <div className='grid grid-cols-1 sm:grid-cols-2 gap-10 sm:gap-2 md:gap-10 w-full h-96 sm:h-52 my-8 lg:h-64 xl:h-72'>
   <div className='aspect-w-4 aspect-h-3 mx-6 sm:mx-2 xl:mx-10'>
-    <Card />
+    <Card name='Commercial' title='Commercial' description='We offer a comprehensive range of commercial electrical services, including advanced wiring systems, safety inspections, and energy-efficient lighting solutions. Tailored to meet the unique needs of businesses and industries.' />
   </div>
   <div className='aspect-w-4 aspect-h-3 mx-6 sm:mx-2 xl:mx-10'>
-    <Card />
+    <Card name='Residential' title='Residential' description='Experience a wide array of residential electrical services, from modern lighting installations and smart home setups to routine maintenance and safety checks. Customized solutions for your home&apos;s electrical needs.' />
   </div>
 </div>
-            {/* -----------HERE----------- */}
-            {/* <div className='flex flex-col justify-around items-center w-full '>
-            <div className='w-5/6 h-44 sm:w-96 sm:h-60 my-6'>
-            <Card />
-            </div>
-            <div className='w-5/6 h-44 sm:w-96 sm:h-60 my-6'>
-            <Card />
-            </div>
-            </div> */}
-             {/* -----------HERE----------- */}
         </div>
     </div>
   )
