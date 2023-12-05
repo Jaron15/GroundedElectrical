5cc593e7d2bc1b434a91b9ee183b6c01ec6e33e7
Jaron15 <jjkenyon777@gmail.com>
2023-12-04 18:15:46 -0700

    style edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
         <a href="mailto:Groundedinaz@gmail.com" className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
             <IoIosMail className='hover:text-white transition ease-in-out duration-300 ' />
             </a>
-        <a href="#" className="inline-block">
+        <a href="https://g.co/kgs/BC6CMh" className="inline-block">
         <div className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 inline-flex items-center justify-center hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
           <div className="filter grayscale transition ease-in-out duration-300 hover:grayscale-0">
             <FcGoogle />
@@ -29,7 +29,6 @@ const Footer = () => {
         <li><a href="#" className="hover:underline">Home</a></li>
         <li><a href="#" className="hover:underline">Services</a></li>
         <li><a href="#" className="hover:underline">About</a></li>
-        <li><a href="#" className="hover:underline">Terms</a></li>
         <li><a href="#" className="hover:underline">Contact</a></li>
       </ul>
       <p className="text-center text-sm text-gray-400">ROC 348796</p>
