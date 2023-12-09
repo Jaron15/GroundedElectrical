e2a9c2df097655945a6b25004d742fe674d6d161
Jaron15 <jjkenyon777@gmail.com>
2023-12-09 12:39:05 -0700

    target blank for google

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
   <IoIosMail className='max-sm:text-white hover:text-white transition ease-in-out duration-300' />
 </a>
 
-<a href="https://g.co/kgs/BC6CMh" className="inline-block">
+<a href="https://g.co/kgs/BC6CMh" target="_blank" className="inline-block">
   <div className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 inline-flex items-center justify-center hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
     <div className="filter max-sm:grayscale-0 grayscale transition ease-in-out duration-300 hover:grayscale-0">
       <FcGoogle />
