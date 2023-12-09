37ae1100fd2fbb444e13585bcd85193537cb9527
Jaron15 <jjkenyon777@gmail.com>
2023-12-09 12:37:21 -0700

    adjusted icons for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,24 +7,29 @@ const Footer = () => {
   return (
       <footer className="bg-black text-gray-600 p-6  border-red-500 w-full">
      
-      <div className="flex justify-center space-x-4 mb-6">
+     <div className="flex justify-center space-x-4 mb-6">
+     <a href="https://www.instagram.com/groundedinAZ" target="_blank" className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
+  <IoLogoInstagram className='max-sm:text-[#CF2872] hover:text-[#CF2872] transition ease-in-out duration-300'/>
+</a>
 
-        <a href="https://www.instagram.com/groundedinAZ" target="_blank"  className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
-            <IoLogoInstagram  className='hover:text-[#CF2872] transition ease-in-out duration-300'/>
-            </a>
-            
-        <a href="mailto:Groundedinaz@gmail.com" className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
-            <IoIosMail className='hover:text-white transition ease-in-out duration-300 ' />
-            </a>
-        <a href="https://g.co/kgs/BC6CMh" className="inline-block">
-        <div className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 inline-flex items-center justify-center hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
-          <div className="filter grayscale transition ease-in-out duration-300 hover:grayscale-0">
-            <FcGoogle />
-          </div>
-        </div>
-      </a>
-        <a href="https://www.facebook.com/groundedinAZ" target="_blank" className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 hover:text-blue-500 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300 "><FaFacebookF /></a>
-      </div>
+<a href="mailto:Groundedinaz@gmail.com" className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
+  <IoIosMail className='max-sm:text-white hover:text-white transition ease-in-out duration-300' />
+</a>
+
+<a href="https://g.co/kgs/BC6CMh" className="inline-block">
+  <div className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 inline-flex items-center justify-center hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
+    <div className="filter max-sm:grayscale-0 grayscale transition ease-in-out duration-300 hover:grayscale-0">
+      <FcGoogle />
+    </div>
+  </div>
+</a>
+
+<a href="https://www.facebook.com/groundedinAZ" target="_blank" className="text-2xl border border-gold rounded-full p-2 hover:opacity-90 max-sm:text-blue-500 hover:text-blue-500 hover:shadow-[0_0_20px_#ffd277] transition ease-in-out duration-300">
+  <FaFacebookF />
+</a>
+
+</div>
+
       <ul className="flex flex-wrap justify-center space-x-4 text-lg mb-6">
         <li><a href="#" className="hover:underline">Home</a></li>
         <li><a href="#" className="hover:underline">Services</a></li>
